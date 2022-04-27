8dbe68f8034103b6c0fe48656aab3ecdd9ff1321
Jamie <jamiejliu8gmail.com>
2022-04-27 17:02:04 -0400

    added fade animation

--- script.js
+++ script.js
@@ -1,40 +1,34 @@
-// const scrollElements = document.querySelectorAll(".js-scroll");
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fade").each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
 
-// const elementInView = (el, dividend = 1) => {
-//   const elementTop = el.getBoundingClientRect().top;
+      /* If the element is completely within bounds of the window, fade it in */
+      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      } else { //object goes out of view (scrolling up)
+        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+      }
+    });
+  }).scroll(); //invoke scroll-handler on page-load
+});
 
-//   return (
-//     elementTop <=
-//     (window.innerHeight || document.documentElement.clientHeight) / dividend
-//   );
-// };
 
-// const elementOutofView = (el) => {
-//   const elementTop = el.getBoundingClientRect().top;
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fadeImage").each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
 
-//   return (
-//     elementTop > (window.innerHeight || document.documentElement.clientHeight)
-//   );
-// };
-
-// const displayScrollElement = (element) => {
-//   element.classList.add("scrolled");
-// };
-
-// const hideScrollElement = (element) => {
-//   element.classList.remove("scrolled");
-// };
-
-// const handleScrollAnimation = () => {
-//   scrollElements.forEach((el) => {
-//     if (elementInView(el, 1.25)) {
-//       displayScrollElement(el);
-//     } else if (elementOutofView(el)) {
-//       hideScrollElement(el)
-//     }
-//   })
-// }
-
-// window.addEventListener("scroll", () => {
-//   handleScrollAnimation();
-// });
+      /* If the element is completely within bounds of the window, fade it in */
+      if (objectBottom/1.5 < windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      } else { //object goes out of view (scrolling up)
+        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+      }
+    });
+  }).scroll(); //invoke scroll-handler on page-load
+});
